3a16b90cef13cd074f12fbba37d8585181e38ec7
aaryadevnani <aarya_devnani@hotmail.com>
2023-03-22 02:51:11 +0530

    Accessed size chart and console logged all sizes

--- background/contentScript.js
+++ background/contentScript.js
@@ -1,1 +1,11 @@
 console.log("Hello from content script")
+let sizeChart = document.getElementsByClassName("size-grid-button")
+console.log(sizeChart)
+for(let i = 0; i < sizeChart.length;i++){
+    console.log(sizeChart[i])
+}
+    
+
+
+
+
