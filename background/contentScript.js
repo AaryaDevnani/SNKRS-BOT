18ba56dcb5eb01c26b9c04deef1949c1818ca83c
Kavish Shah <kavishshah09@gmail.com>
2023-03-23 18:31:53 +0530

    formatting and syntax changes

--- background/contentScript.js
+++ background/contentScript.js
@@ -1,36 +1,18 @@
-console.log("Hello from content script")
+console.log("Hello from content script");
 
-function selectSize(sizeString){
-
-    let sizeChart = document.getElementsByClassName("size-grid-button")
-    
-    console.log(sizeChart)
-    for(let i = 0; i < sizeChart.length;i++){
-        // console.log(sizeChart[i].innerText)
-        let sizeText = sizeChart[i].innerText
-        console.log("Click click")
-        if(sizeText.includes(sizeString)){
-            sizeChart[i].click()
-            let buyBtn = document.getElementsByClassName("buying-tools-cta-button")[0]
-            console.log(buyBtn)
-            console.log(buyBtn.click())
-            // buyBtn.click()
-            break;
-        }
+function selectSize(sizeString) {
+  let sizeChart = document.getElementsByClassName("size-grid-button");
+  Array.from(sizeChart).forEach(size => {
+    if (size.innerText.includes(sizeString)) {
+        size.click()
+        let buyBtn = document.getElementsByClassName("buying-tools-cta-button")[0];
+        buyBtn.click();
     }
-    // console.log(buyBtn)
-    
+  })
 }
 
-var loadfunction = window.onload;
-window.onload = function(event){
-    //enter here the action you want to do once loaded
-    selectSize("M 4.5 / W 6")
-    if(loadfunction) loadfunction(event);
-}
-
-    
-
-
-
-
+let loadfunction = window.onload;
+window.onload = function (event) {
+  selectSize("M 8.5");
+  if (loadfunction) loadfunction(event);
+};
