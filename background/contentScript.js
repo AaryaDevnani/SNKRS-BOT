6385b322ad154e17819827489f1a0322219090a0
aaryadevnani <aarya_devnani@hotmail.com>
2023-03-23 16:08:00 +0530

    Size Button click

--- background/contentScript.js
+++ background/contentScript.js
@@ -1,9 +1,22 @@
 console.log("Hello from content script")
-let sizeChart = document.getElementsByClassName("size-grid-button")
-console.log(sizeChart)
-for(let i = 0; i < sizeChart.length;i++){
-    console.log(sizeChart[i])
+
+function selectSize(sizeString){
+
+    let sizeChart = document.getElementsByClassName("size-grid-button")
+    let buyBtn = document.getElementsByClassName("buying-tools-cta-button")[0]
+    console.log(sizeChart)
+    for(let i = 0; i < sizeChart.length;i++){
+        // console.log(sizeChart[i].innerText)
+        let sizeText = sizeChart[i].innerText
+        if(sizeText.includes(sizeString)){
+            console.log("Click click")
+            sizeChart[i].click()
+        }
+    }
+    console.log(buyBtn)
+    buyBtn.click()
 }
+selectSize("M 4 / W 5.5")
     
 
 
